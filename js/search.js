e980a6c5bc9c372a451ca7eb3a41c919900f513f
hobbang666 <harryshin99@naver.com>
2025-05-02 01:23:49 +0900

    자바 웹 프로그래밍 4주차 연습문제

---
+++ js/search.js
@@ -0,0 +1,10 @@
+document.getElementById("search_button_msg").addEventListener('click', search_message);
+function search_message(){
+let message = "검색을 시작합니다!!!"
+alert(message);
+}
+/*function search_message(){
+    alert("검색합니다");
+}
+    */
+    
