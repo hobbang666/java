5fd2c4cb47ae97d86b694aa44132027f8912b870
hobbang666 <harryshin99@naver.com>
2025-05-30 00:53:09 +0900

    13주차 최종수정5

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,5 +1,5 @@
 function pop_up() {
-    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    window.open("/java/popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
 }
 
 function show_clock(){
