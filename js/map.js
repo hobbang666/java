94e235abdfe85f8c62cd1cc807c693b6562e6c9d
hobbang666 <harryshin99@naver.com>
2025-05-30 13:55:56 +0900

    13주차 최종수정 40

--- js/map.js
+++ js/map.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  kakao.maps.load(function () {
-    var mapContainer = document.getElementById('map'),
-        mapOption = {
-            center: new kakao.maps.LatLng(37.566826, 126.9786567),
-            level: 3
-        };
+  var mapContainer = document.getElementById('map'),
+      mapOption = {
+          center: new kakao.maps.LatLng(37.566826, 126.9786567),
+          level: 3
+      };
 
     var map = new kakao.maps.Map(mapContainer, mapOption);
     var ps = new kakao.maps.services.Places();
